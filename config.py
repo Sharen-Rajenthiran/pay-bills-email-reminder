acec7ecbc05a0a991dd675ebff5c1126d36ed652
Sharen Rajenthiran <131507266+Sharen-Rajenthiran@users.noreply.github.com>
2025-07-20 12:28:32 +0800

    Create and update config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+from decouple import config
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    SENDER: str = config("SENDER")
+    RECEIVER: str = config("RECEIVER")
+    PASSWORD: str = config("GOOGLE_APP_PASSWORD")
+    GE: str = config("GE")
+
+    class Config:
+        case_sensitive = True
+
+settings = Settings()
