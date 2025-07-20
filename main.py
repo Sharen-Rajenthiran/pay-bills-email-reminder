e72dc15fc6f6df18aa187b70e4e86df9922fd11d
Sharen Rajenthiran <131507266+Sharen-Rajenthiran@users.noreply.github.com>
2025-07-20 12:27:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from create_message import create_message
+from send_message import send_message
+import logging_config
+import logging
+import os
+
+LOG = logging.getLogger(os.path.basename(__file__))
+
+if __name__ == "__main__":
+    try:
+        created_message = create_message()
+        LOG.info("message created") 
+        send_message(created_message)
+        LOG.info("message sucessfully sent!")
+    except Exception as e:
+        LOG.error("Error sending message {e}")
+
 
